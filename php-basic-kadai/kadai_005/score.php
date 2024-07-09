2917a0c9cdd117e01f14b0d1a24bf741d6298973
m-0518h <velvet.touch303@gmail.com>
2024-07-09 21:08:01 +0900

    Add files via upload

---
+++ php-basic-kadai/kadai_005/score.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP課題005</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+    echo $score1 = 80;
+    echo "<br>";
+    echo $score2 = 60;
+    echo "<br>";
+    echo $score3 = 55;
+    echo "<br>";
+    echo $score4 = 40;
+    echo "<br>";
+    echo $score5 = 100;
+    echo "<br>";
+    echo $score6 = 25;
+    echo "<br>";
+    echo $score7 = 80;
+    echo "<br>";
+    echo $score8 = 95;
+    echo "<br>";
+    echo $score9 = 30;
+    echo "<br>";
+    echo $score10 = 60;
+    echo "<br>";
+
+    echo "合計は";
+    echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    echo "<br>";
+    echo "平均は";
+    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+
+
+
+
+    ?>
+    
+    
+  </body>
+</html>
