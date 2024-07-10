9ee1fc10533ee0910b0c0acc0dacf0f5dac7681b
m-0518h <velvet.touch303@gmail.com>
2024-07-10 20:57:52 +0900

    Add files via upload

---
+++ php-basic-kadai/kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP課題007</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p>
+      <?php
+      $vegetable = ["name" => "onion", "price" => 200, "weight" => 160];
+      print_r($vegetable);
+      ?>
+    </p>
+    
+  </body>
+</html>
