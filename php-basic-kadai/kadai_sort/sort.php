823d7a9e5645be9f3784927e458a9c79dc6b5c7d
m-0518h <velvet.touch303@gmail.com>
2024-07-14 06:17:36 +0900

    Add files via upload

---
+++ php-basic-kadai/kadai_sort/sort.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP課題sort</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p>
+      <?php
+      $array = [15, 4, 18, 23, 10];
+
+
+
+      function sort_2way($array, $order){
+
+        //TRUE
+        if($order == TRUE){
+          echo "昇順にソートします。<br>";
+          sort($array);
+          foreach($array as $key => $val) {
+            echo "{$val} <br>";
+          }
+
+        //FALSE
+        } else {
+          echo "降順にソートします。<br>";
+          rsort($array);
+          foreach($array as $key => $val) {
+            echo "{$val} <br>";
+          }
+        }    
+      }
+
+      sort_2way($array, FALSE);
+
+
+
+      
+
+
+      ?>
+    </p>
+    
+  </body>
+</html>
